dceda1dc08c96edc5c6f7acc07458a58316e7002
asimzubair <asimzubair7@gmail.com>
2022-07-03 20:46:11 +0500

    Search form added and testing AJAX

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,17 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends AbstractController
 {
     /**
-     * @Route("/search", name="app_search")
+     * @Route("/", name="search_form")
      */
-    public function index(): JsonResponse
+    public function index()
     {
+        return $this->render('search.html.twig');
+    }
+
+    /**
+     * @Route("/search", name="search_json")
+     */
+    public function search(Request $request)
+    {
+        //$request->get('startDate');
+        //$request->get('endDate');
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SearchController.php',
+        	$request->get('companySymbol')
         ]);
     }
 }
